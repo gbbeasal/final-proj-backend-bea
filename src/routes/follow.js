a17e73c59c7d1a3d662d0afc4f8393900db8bb02
BSal_Windows <ma.salazar@globe.com.ph>
2022-12-06 11:23:26 +0800

    Implemented list followers and ppl im following

--- src/routes/follow.js
+++ src/routes/follow.js
@@ -4,48 +4,85 @@ import omit from 'lodash/omit.js';
 
 const followRouter = express.Router();
 
-// ============ GETTING ALL FAVORITES ============:
+// ============ GETTING ALL USERS I FOLLOW ============:
 // Authenticated User = can see their tweets
 // Unauthenticated/Invalid JWT Session User = will be prompted to login
 
-// followRouter.get('/myfavorites', async (request, response) => {
-//   const cookies = request.cookies;
-//   const jwtSession = cookies.sessionId;
+followRouter.get('/usersifollow', async (request, response) => {
+  const cookies = request.cookies;
+  const jwtSession = cookies.sessionId;
 
-//   if (!jwtSession) {
-//     response
-//       .status(401)
-//       .send({ data: null, message: 'Invalid Request - Please login' });
-//     return;
-//   }
+  if (!jwtSession) {
+    response
+      .status(401)
+      .send({ data: null, message: 'Invalid Request - Please login' });
+    return;
+  }
 
-//   try {
-//     const jwtSessionObject = await jwt.verify(
-//       jwtSession,
-//       process.env.JWT_SECRET
-//     );
-//     const userId = jwtSessionObject.uid;
+  try {
+    const jwtSessionObject = await jwt.verify(
+      jwtSession,
+      process.env.JWT_SECRET
+    );
+    const userId = jwtSessionObject.uid;
 
-//     const favorites = await request.app.locals.prisma.favorite.findMany({
-//       orderBy: {
-//         createdAt: 'desc',
-//       },
-//       where: { userId: userId },
-//       include: {
-//         tweet: true,
-//       },
-//     });
+    const following = await request.app.locals.prisma.follow.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      where: { followerId: userId },
+    });
 
-//     response.send({
-//       favorites: favorites,
-//       message: favorites ? 'ok' : 'no replies found',
-//     });
-//   } catch {
-//     response
-//       .status(401)
-//       .send({ data: null, message: 'Invalid Request - Please try again' });
-//   }
-// });
+    response.send({
+      usersIFollow: following,
+      message: following ? 'ok' : 'You are not following anyone',
+    });
+  } catch {
+    response
+      .status(401)
+      .send({ data: null, message: 'Invalid Request - Please try again' });
+  }
+});
+
+// ============ GETTING ALL USERS THAT FOLLOW ME ============:
+// Authenticated User = can see their tweets
+// Unauthenticated/Invalid JWT Session User = will be prompted to login
+
+followRouter.get('/usersthatfollowme', async (request, response) => {
+  const cookies = request.cookies;
+  const jwtSession = cookies.sessionId;
+
+  if (!jwtSession) {
+    response
+      .status(401)
+      .send({ data: null, message: 'Invalid Request - Please login' });
+    return;
+  }
+
+  try {
+    const jwtSessionObject = await jwt.verify(
+      jwtSession,
+      process.env.JWT_SECRET
+    );
+    const userId = jwtSessionObject.uid;
+
+    const followers = await request.app.locals.prisma.follow.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      where: { followingId: userId },
+    });
+
+    response.send({
+      Followers: followers,
+      message: followers ? 'ok' : 'You are not following anyone',
+    });
+  } catch {
+    response
+      .status(401)
+      .send({ data: null, message: 'Invalid Request - Please try again' });
+  }
+});
 
 // ============ FOLLOW AND UNFOLLOW ============:
 // Authenticated User = can follow
@@ -77,9 +114,10 @@ followRouter.put('/:userName/follow', async (request, response) => {
     // cannot follow self:
 
     if (userId == userToFollow.id) {
-      response
-        .status(401)
-        .send({ data: null, message: 'Invalid Request - You cannot follow yourself' });
+      response.status(401).send({
+        data: null,
+        message: 'Invalid Request - You cannot follow yourself',
+      });
       return;
     }
 
